0e5efaf9c5d937c2c2a28d0dc1790483f907378c
harrisonhall <harrisonchristianhall@gmail.com>
2019-07-14 05:05:30 +0100

    Comms work!

--- modules/Comm.py
+++ modules/Comm.py
@@ -1,12 +1,21 @@
 """Perform communication related functions."""
+from digi.xbee.devices import XBeeDevice
 
 class Comm:
     def __init__(self):
-        pass
+        self.device = XBeeDevice("/dev/ttyUSB0", 9600) # TODO assumes USB0
+        self.active = True
+        try:
+            self.device.open()
+        except:
+            self.active = False
+        return None
 
     def read_comm(self, rocket_state):
         """Read communication for meta-state changes."""
         return rocket_state
 
     def send(self,data):
-        pass
+        """Broadcast data on network."""
+        self.device.send_data_broadcast(data)
+        return None
