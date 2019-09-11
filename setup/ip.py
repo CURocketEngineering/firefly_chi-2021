8e12bc57f8e3c2cf9db8157f4b76204643a4ee20
harrisonhall <harrisonchristianhall@gmail.com>
2019-09-11 08:05:43 -0400

    print ip

--- setup/ip.py
+++ setup/ip.py
@@ -1,8 +1,11 @@
+"""Print IP Address to shell and sensehat."""
+
 from sense_hat import SenseHat
 import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.connect(("8.8.8.8", 80))
 ip = s.getsockname()[0]
+print(ip)
 SenseHat().show_message(ip)
 
