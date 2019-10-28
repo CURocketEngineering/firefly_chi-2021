ea117f930d19c21b4dca46bc2217ebb0b53eaba6
harrisonhall <harrisonchristianhall@gmail.com>
2019-10-28 11:10:47 -0400

    begin sensehat interface

---
+++ sensehat_interface.py
@@ -0,0 +1,8 @@
+"""Used to run avionics from sensehat."""
+
+from sense_hat import SenseHat
+import avionics
+
+if __name__ == "__main__":
+    input("DEBUG: Press <ENTER> to continue.")
+    avionics.avionics()
