788bd74065d3d01b525bcd9c761eb3203c19e9ff
harrisonhall <harrisonchristianhall@gmail.com>
2021-03-03 19:39:12 -0500

    Code to generate records folder for flight data

--- plugins/FileLogging.py
+++ plugins/FileLogging.py
@@ -12,22 +12,28 @@ most important data would allow anyone to modify write speeds to favor more
 demanding data analysis. More data = more accurate flight analysis. - Harrison
 """
 
-from time import sleep
+
 from json import dumps
+from os import path, makedirs
+from time import sleep
 
 FILELOGGING_DT = 0.2  # Log data 5 times per second
 FILECLOSING_DT = 10  # Every 10 seconds, close and open file
 REDUNDANCY_COUNT = 3  # Triple redundancy as per NASA https://llis.nasa.gov/lesson/18803
+FILE_DIRECTORY = "records"
 
 def FileLogger(conf, data):
     files = []
+    # Make folder if not exists
+    if not path.exists(FILE_DIRECTORY):
+        makedirs(FILE_DIRECTORY)
     try:
         while True:
             # Open files
             files = []
             for i in range(REDUNDANCY_COUNT):
                 modifier = str(i) if i > 0 else ""
-                files.append(open(f"records/output{modifier}.json", "w"))
+                files.append(open(f"{FILE_DIRECTORY}/output{modifier}.json", "w"))
             # Log
             for i in range(int(FILECLOSING_DT/FILELOGGING_DT)):
                 d = dumps(data.to_dict())
